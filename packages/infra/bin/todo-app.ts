65089dcd8ef8fd5f2a6b2a219249e2d3bb857a27
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2021-03-04 22:27:47 -0600

    feat(skeleton): Adding default region and account to setup

--- packages/infra/bin/todo-app.ts
+++ packages/infra/bin/todo-app.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { TodoAppStack } from '../lib/todo-app-stack';
 
 const app = new cdk.App();
-new TodoAppStack(app, 'todo-app');
+new TodoAppStack(app, 'todo-app', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
+});

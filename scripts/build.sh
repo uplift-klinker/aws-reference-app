cf8459468cc0216777192b30ab7802b88467605e
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2021-03-04 22:04:31 -0600

    feat(skeleton): adding deployment to the actual app

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,16 +2,22 @@
 
 set -ex
 
+export INFRASTRUCTURE_DIRECTORY="./packages/infra"
+
 function install() {
   yarn install
 }
 
 function run_tests() {
+  yarn build
   yarn test
 }
 
 function deploy() {
-  yarn build
+  pushd "${INFRASTRUCTURE_DIRECTORY}" || exit 1
+  yarn cdk synth
+  yarn cdk deploy
+  popd || exit 1
 }
 
 function main() {

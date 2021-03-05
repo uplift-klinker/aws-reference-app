ea69ea6c1ee998d28598bf72b0a8e62a9cda2d58
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2021-03-04 23:59:27 -0600

    feat(skeleton): Printing region to make sure node script can see that

--- packages/infra/lib/upload-settings.ts
+++ packages/infra/lib/upload-settings.ts
@@ -3,6 +3,8 @@ import {STACK_NAME} from "./stack-name";
 import {OUTPUTS} from "./outputs";
 
 const region = process.env.CDK_DEFAULT_REGION || 'us-east-1';
+console.log(`Using region ${region}`);
+
 const cloudFormation = new CloudFormation({region});
 const s3 = new S3({region});
 
